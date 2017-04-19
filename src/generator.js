bf90986d8ad0edaadb85c599e7ad5a24ef2fa976
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-04-19 17:35:11 +0200

    add generator

--- src/generator.js
+++ src/generator.js
@@ -1,3 +1,31 @@
 
-// waiting for es6
-// TODO it's 2017, it's there, do it!
+
+export function __generator__ ( iadd , zero , one ) {
+
+	const generator = function* ( ) {
+
+		let a = zero( ) ;
+
+		yield a;
+
+		let b = one( ) ;
+
+		yield b;
+
+		while ( true ) {
+
+			a = iadd( a , b ) ;
+
+			yield a;
+
+			b = iadd( b , a ) ;
+
+			yield b;
+
+		}
+
+	} ;
+
+	return generator ;
+
+}
---
+++ test/src/generator.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+import * as number from '@aureooms/js-number' ;
+import { range } from '@aureooms/js-itertools' ;
+
+import * as fibonacci from '../../src';
+
+test( 'generator' , t => {
+
+	const generator = fibonacci.__generator__( number.iadd , number.$0 , number.$1 ) ;
+
+	const f = generator();
+
+	const n = 47;
+
+	let a, b, c;
+
+	for (let i of range(n)) {
+
+
+		if ( i === 0 ) {
+
+			a = number.$0( ) ;
+
+			t.truthy( number.eq( f.next().value , a ) ) ;
+
+		}
+
+		else if ( i === 1 ) {
+
+			b = number.$1( ) ;
+
+			t.truthy( number.eq( f.next().value , b ) ) ;
+
+		}
+
+		else {
+
+			c = number.add( a , b ) ;
+
+			t.truthy( number.eq( f.next().value , c ) ) ;
+
+			a = b ;
+			b = c ;
+
+		}
+
+	} ;
+
+} ) ;
